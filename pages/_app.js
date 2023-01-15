d4de74a89700daebb6820bd1964f027392b5ea0a
Daniel Mbengui <couzmec@macbook-air-de-daniel.home>
2023-01-15 04:56:00 +0100

    google adsense tag

--- pages/_app.js
+++ pages/_app.js
@@ -71,7 +71,7 @@ const App = (props) => {
         </ThemeProvider>
       </LocalizationProvider>
 
-      <ins className="adsbygoogle"
+      <ins class="adsbygoogle"
         style={{display:"block", textAlign:"center"}}
         data-ad-layout="in-article"
         data-ad-format="fluid"
