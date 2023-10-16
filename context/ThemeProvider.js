8edbd0d09f9abc89ca9cec52f4aa79ae84ca6013
Daniel Mbengui <couzmec@macbook-air-de-daniel.home>
2023-10-16 11:21:57 +0200

    update sitemap

--- context/ThemeProvider.js
+++ context/ThemeProvider.js
@@ -305,9 +305,9 @@ export default function ThemeModeProvider({ children, screenMode }) {
                 100: '#F3F4F6',
                 200: '#E5E7EB',
                 300: '#D1D5DB',
-                400: '#9CA3AF',
+                400: neutral400,
                 500: '#6B7280',
-                600: '#4B5563',
+                600: neutral600,
                 700: '#374151',
                 800: '#1F2937',
                 900: '#000c25'
